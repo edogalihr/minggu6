fb986ae62fa20efa55f39f25f0b0fe4895b03867
edogalihr <edogalihr7@gmail.com>
2021-10-18 19:21:07 +0700

    UTS_C

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\userController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,4 +23,10 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::get('/searchUser', [userController::class, 'searchUser'])->name('searchUser');
+
+Route::get('/search', [studentController::class, 'search'])->name('search');
+
 Route::resource('students', StudentController::class);
+
+Route::resource('userCrud', userController::class);
