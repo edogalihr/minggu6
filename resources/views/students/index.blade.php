f69ff5412ce3199cd46bb938fb4f9497f62b312b
edogalihr <edogalihr7@gmail.com>
2021-10-18 19:19:45 +0700

    UTS_C

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -17,6 +17,13 @@
                     <a href="/students/create" class="btn btn-primary">Add Data</a> 
                     <br><br>
 
+                    <form class="form" method="get" action="{{ route('search') }}">
+                    <div class="form-group w-100 mb-3">
+                    <label for="search" class="d-block mr-2">Pencarian</label>
+                    <input type="text" name="search" class="form-control w-75 d-inline" id="search" placeholder="Search Data">
+                    <button type="submit" class="btn btn-primary mb-1">Cari</button>
+                    </div></form>
+
                     <table class="table table-responsive table-striped">
                         <thead>
                             <tr>
@@ -37,7 +44,7 @@
                                 <td>
                                     <form action="/students/{{$s->id}}" method="post">
                                         <a href="/students/{{$s->id}}/edit" class="btn btn-warning">Edit</a>
-
+                                        <a href="/students/{{$s->id}}" class="btn btn-warning">View</a>
                                         @csrf
                                         @method('DELETE')
                                         <button type="submit" name="delete" class="btn btn-danger">Delete</button>
